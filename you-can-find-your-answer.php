3a9364d96c924a8a3f093f1e03cc56a8e0af4395
Marcin Lenkowski <marcin@lenkowski.net>
2022-05-18 20:38:16 +0200

    can you find me?

---
+++ you-can-find-your-answer.php
@@ -0,0 +1,40 @@
+<?php
+
+$str = '5+5/2*3';
+preg_match_all('!\d+!', $str, $nums);
+preg_match_all('!\D+!', $str, $operators);
+
+$operators = $operators[0];
+$nums = $nums[0];
+
+$result = 0;
+foreach ($operators as $key => $operator) {
+    if ($result === 0) {
+        $result = $nums[$key];
+    }
+
+    switch ($operator) {
+        case '+':
+            $result += $nums[$key + 1];
+            break;
+        case '-':
+            $result -= $nums[$key + 1];
+            break;
+        case '*':
+            if ($result == 0) {
+                $result = 1;
+            }
+            $result *= $nums[$key + 1];
+            break;
+        case '/':
+            if ($result == 0) {
+                $result = 1;
+            }
+            $result /= $nums[$key + 1];
+            break;
+    }
+}
+
+echo $result;
+
+// this misses some of the task part, but contains the essencial solution!
